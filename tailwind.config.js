0387881e0e600d21499ecdd941096ea16a9127ae
John <komarnickij@yahoo.com>
2024-12-07 11:34:02 -0500

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import { colors } from "tailwindcss/colors";
+
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     colors: {
